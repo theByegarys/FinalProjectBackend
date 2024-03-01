cf3c0db95a1b52ba452ce527ff45ff74b18f7299
theByegarys <151679453+theByegarys@users.noreply.github.com>
2024-03-01 07:29:58 +0600

    Add files via upload

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,122 @@
+import React, { useState, useContext } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import axios from "../Axios/axios.js"
+import TokenContext from '../context/TokenContext.js';
+function Login() {
+    const [formData, setFormData] = useState({});
+    const { userToken, tokenDispatch, userDispatch } = useContext(TokenContext);
+    const [error, setError] = useState();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const result = await axios.post("/user/login", formData)
+            tokenDispatch({ type: "SET_TOKEN", payload: result.data.token })
+            userDispatch({ type: "SET_USER", payload: result.data.user })
+            localStorage.setItem("authToken",JSON.stringify(result.data.token))
+        } catch (error) {
+            console.log(error);
+            setError({ message: error.response.data.message })
+        }
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value })
+    }
+    return (
+        <div>
+            {userToken && <Navigate to="/" />}
+            <section className="login-container">
+                <div className="px-6 h-full text-gray-800">
+                    <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
+                        <div className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
+                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="w-full" alt="Sample" />
+                        </div>
+                        <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+                            <form method='post' onSubmit={handleSubmit}>
+                                <div className="flex flex-row items-center justify-center lg:justify-start">
+                                    <p className="text-lg mb-0 mr-4">Sign in with</p>
+                                    {/* <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" className="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1">
+                                        Facebook
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="w-4 h-4">
+                                            ! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc.
+                                            <path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
+                                        </svg>
+                                    </button> */}
+                                    <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" className="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1" >
+                                        {/* Linkedin */}
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-4 h-4">
+                                            {/*! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}
+                                            <path fill="currentColor" d="M0 57.69v391.812L298.066 512V0zm150.167 287.028c-94.754-6.111-84.995-176.906 2.212-178.263c93.423 6.175 84.258 176.9-2.212 178.263m1.366-144.822c-49.919 3.466-47.684 110.407-.77 111.265c49.704-3.203 46.785-110.434.77-111.265m197.32 68.113c4.5 3.308 9.922 0 9.922 0c-5.404 3.308 147.63-98.342 147.63-98.342v184.07c0 20.037-12.827 28.441-27.25 28.441H316.892l.01-136.11zM316.91 108.554v100.15l34.999 22.037c.923.27 2.923.289 3.846 0l150.629-101.554c0-12.018-11.211-20.633-17.538-20.633z" />
+                                        </svg>
+                                    </button>
+                                </div>
+                                <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+                                    <p className="text-center font-semibold mx-4 mb-0">Or</p>
+                                </div>
+                                <div>
+                                    {error && (
+                                        <div className="text-center border-2 border-green-600 p-2 mb-2 rounded-md bg-red-200 shadow-2xl">
+                                            {error.message}
+                                        </div>
+                                    )
+                                    }
+                                </div>
+                                {/* Email input */}
+                                <div className="mb-6">
+                                    <input
+                                        type="text"
+                                        name='email'
+                                        onChange={handleChange}
+                                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                        id="emailInput"
+                                        placeholder="Email address" />
+                                </div>
+                                {/* Password input */}
+                                <div className="mb-6">
+                                    <input
+                                        type="password"
+                                        name='password'
+                                        onChange={handleChange}
+                                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                        id="passInput"
+                                        placeholder="Password" />
+                                </div>
+                                <div className="flex justify-between items-center mb-6">
+                                    <div className="form-group form-check">
+                                        <input
+                                            type="checkbox"
+                                            className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                            id="exampleCheck2" />
+                                        <label className="form-check-label inline-block text-gray-800" htmlFor="exampleCheck2">Remember me</label>
+                                    </div>
+                                    <Link
+                                        to={"/forgotPassword"}
+                                    >Forgot Password</Link>
+                                </div>
+                                <div className="text-center lg:text-left">
+                                    <button
+                                        type="submit"
+                                        onClick={handleSubmit}
+                                        className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                                        Login
+                                    </button>
+                                    <p className="text-sm font-semibold mt-2 pt-1 mb-0">
+                                        Don't have an account?
+                                        <Link
+                                            to={"/register"}
+                                            className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+                                        > Register Right Now!</Link>
+                                    </p>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    );
+}
+
+export default Login;
